dc4cdfd3a55f601478cd075b7e108baa69a375b9
Hoang Nguyen <nhsh1997@gmail.com>
2020-04-28 14:05:33 +0700

    Update items_service.go

--- services/items_service.go
+++ services/items_service.go
@@ -1,8 +1,25 @@
 package services
 
-import "github.com/nhsh1997/bookstore_items-api/domain/items"
+import (
+	"github.com/nhsh1997/bookstore_items-api/domain/items"
+)
 import "github.com/nhsh1997/bookstore_utils-go/rest_errors"
 
+var(
+	ItemService itemServiceInterface = &itemService{}
+)
+
 type itemServiceInterface interface {
-	Create(items.Item) (*items.Item, rest_errors.RestError)
+	Create(items.Item) (*items.Item, *rest_errors.RestError)
+	Get(string) (*items.Item, *rest_errors.RestError)
+}
+
+type itemService struct{}
+
+func (s *itemService) Create(items.Item) (*items.Item, *rest_errors.RestError) {
+	return nil, rest_errors.NewNotImplementedError("implement me")
+}
+
+func (s *itemService) Get(string) (*items.Item, *rest_errors.RestError) {
+	return nil, rest_errors.NewNotImplementedError("implement me")
 }
